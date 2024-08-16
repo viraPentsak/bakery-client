1719fea14809155ddcf0f6698c181d429e054666
viraPentsak <61148110+viraPentsak@users.noreply.github.com>
2024-08-16 16:32:53 +0300

    Adds settings and packages

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,29 +1,84 @@
-import type { GatsbyConfig } from "gatsby";
+import type {GatsbyConfig} from "gatsby";
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `bakery`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `bakery`,
+        menuLinks: [
+            {
+                name: "home",
+                link: "/"
+            },
+            {
+                name: "menu",
+                link: "/menu"
+            },
+            {
+                name: "news",
+                link: "/news"
+            },
+            {
+                name: "contact",
+                link: "/contact"
+            }
+        ],
+        socialLinks: [
+            {
+                name: "twitter",
+                link: "www.twitter.com/bakery"
+            },
+            {
+                name: "facebook",
+                link: "www.facebook.com/bakery"
+            },
+            {
+                name: "instagram",
+                link: "www.instagram.com/bakery"
+            }
+        ]
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+    // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+    // If you use VSCode you can also use the GraphQL plugin
+    // Learn more at: https://gatsby.dev/graphql-typegen
+    graphqlTypegen: true,
+    plugins: [
+        "gatsby-plugin-postcss",
+        "gatsby-plugin-image",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                "name": "images",
+                "path": "./src/images/"
+            },
+            __key: "images"
+        }, {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                "name": "pages",
+                "path": "./src/pages/"
+            },
+            __key: "pages"
+        },
+        {
+            resolve: "gatsby-omni-font-loader",
+            options: {
+                mode: "async",
+                enableListener: true,
+                preconnect: ["https://fonts.gstatic.com"],
+                web:[
+                    {
+                        name: "Alegreya",
+                        file: "https://fonts.googleapis.com/css2?family=Alegreya:wght@400;700"
+                    },
+                    {
+                        name: "Roboto",
+                        file: "https://fonts.googleapis.com/css2?family=Roboto:wght@400;700"
+                    }
+                ]
+            }
+        }
+    ]
 };
 
 export default config;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
 module.exports = {
-  content: [
-    `./src/pages/**/*.{js,jsx,ts,tsx}`,
-    `./src/components/**/*.{js,jsx,ts,tsx}`,
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        `./src/pages/**/*.{js,jsx,ts,tsx}`,
+        `./src/components/**/*.{js,jsx,ts,tsx}`,
+    ],
+    theme: {
+        extend: {
+            zIndex:{
+                'header': '100'
+            },
+            fontFamily:{
+                'sans': ['Roboto', ...defaultTheme.fontFamily.sans],
+                'serif': ['Alegreya', ...defaultTheme.fontFamily.serif],
+            }
+        },
+        container: {center: true}
+    },
+    plugins: [],
 }
