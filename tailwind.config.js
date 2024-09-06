62240207d621ce3d5d8a362ba7fbe7107f8314d3
viraPentsak <61148110+viraPentsak@users.noreply.github.com>
2024-09-06 14:37:11 +0300

    Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,11 +16,72 @@ module.exports = {
                 'serif': ['Alegreya', ...defaultTheme.fontFamily.serif],
                 'cursive': ['Great Vibes', 'Alegreya', ...defaultTheme.fontFamily.serif]
             },
-            animation:{
-                "spin-slow":'spin 30s linear infinite'
+            animation: {
+                'spin-slow': 'spin 30s linear infinite'
+            },
+            backgroundImage: {
+                'banner-menu': 'url(./../images/banner-menu.jpg)',
+                'banner-featured': 'url(./../images/banner-featured.jpg)',
+                'windmill': './src/images/bg-windmill.png',
+                'village': './src/images/bg-village.png',
+            },
+            colors: {
+                'straw': {
+                    '50': '#fbf9f1',
+                    '100': '#f6f0de',
+                    '200': '#ecdebc',
+                    '300': '#dfc691',
+                    '400': '#d1a866',
+                    '500': '#c89247',
+                    '600': '#ba7c3c',
+                    '700': '#9a6334',
+                    '800': '#7c5030',
+                    '900': '#654229',
+                    '950': '#362114',
+                },
+                'gray': {
+                    '50': '#f7f7f6',
+                    '100': '#e5e3e2',
+                    '200': '#cbc9c4',
+                    '300': '#a9a69f',
+                    '400': '#87827a',
+                    '500': '#6c6860',
+                    '600': '#56534c',
+                    '700': '#46453f',
+                    '800': '#3a3935',
+                    '900': '#33312e',
+                    '950': '#1b1a18',
+                    'DEFAULT' : '#56534c'
+                },
+                'leather': {
+                    '50': '#f6f4f0',
+                    '100': '#e9e2d8',
+                    '200': '#d5c5b3',
+                    '300': '#bca188',
+                    '400': '#a47d5e',
+                    '500': '#9a7258',
+                    '600': '#845c4a',
+                    '700': '#6a483e',
+                    '800': '#5b3f38',
+                    '900': '#4f3734',
+                    '950': '#2d1d1b',
+                },
+                'laser': {
+                    '50': '#faf8f2',
+                    '100': '#f4f0e0',
+                    '200': '#e7dfc1',
+                    '300': '#d8c999',
+                    '400': '#c9b176',
+                    '500': '#bb9954',
+                    '600': '#ad8549',
+                    '700': '#906b3e',
+                    '800': '#755737',
+                    '900': '#5f482f',
+                    '950': '#332417',
+                },
             }
         },
-        container: {center: true}
+        container: {center: true},
     },
     plugins: [],
 }
